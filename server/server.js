7d0ee40cbb75348e9c8ef381a2124ba632862abb
Divakarreddy2004 <divakarsanikommu26@gmail.com>
2026-01-19 02:24:52 +0530

    dev: temp console changes

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Changed again to - Shutting down gracefully...');
+  console.log('complete changes to application -- Shutting down gracefully...');
   await mongoose.connection.close();
   process.exit(0);
 });
